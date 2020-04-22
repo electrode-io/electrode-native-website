984bad34cfc6f45b5b3a307b08b59df1881a3217
Frieder Bluemle <frieder.bluemle@walmartlabs.com>
2020-04-22 01:00:29 -0700

    Update site config basics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -6,17 +6,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Electrode Native', // Title for your website.
+  tagline: '',
+  url: 'https://www.electrodenative.dev', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'electrode-native-website',
+  organizationName: 'electrode-io',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -37,8 +37,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#3a472a',
-    secondaryColor: '#28311d',
+    primaryColor: '#183055',
+    secondaryColor: '#f47c24',
   },
 
   /* Custom fonts for website */
@@ -56,7 +56,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Walmart Labs`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
