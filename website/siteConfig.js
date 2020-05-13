757ae35d846fce2e115439328e08dde04b2a7eab
Frieder Bluemle <frieder.bluemle@walmartlabs.com>
2020-05-13 13:56:20 -0700

    Set base url to electrode-native-website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -17,7 +17,7 @@ const siteConfig = {
   title: 'Electrode Native', // Title for your website.
   tagline: '',
   url: 'https://www.electrodenative.dev', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/electrode-native-website/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
